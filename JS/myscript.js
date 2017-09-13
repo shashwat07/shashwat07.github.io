d4bfff99b41907ba80819c1ca97b8d2ced57052a
Shashwat Sinha <shashwat.bits@gmail.com>
2017-09-13 15:42:42 +0530

    Gallery takes images form json file

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,9 +1,10 @@
 
+
 function validateForm(){
 	var name = document.forms["contactMeForm"]["name"].value;
 	var email = document.forms["contactMeForm"]["mail"].value;
 	var comment = document.forms["contactMeForm"]["comment"].value;
-	
+
 	if(name==""){
 		alert("Must enter name!");
 		return false;
@@ -13,9 +14,34 @@ function validateForm(){
 		alert("Please enter your email address!");
 		return false;
 	}
-	
+
 	if(comment==""){
 		alert("Hey, you forgot to put in some text to send!");
 		return false;
 	}
 }
+
+var obj = JSON.parse(data);
+
+
+
+var img = obj.images;
+var txt="";
+for(var index=0; index<img.length;index++){
+	var url = obj.images[index].url;
+	var alt = obj.images[index].alt;
+	var info = obj.images[index].info;
+	txt += "<div class=\"floated_img col-lg-4 col-md-6 gallImg\" >";
+	txt += "<img src=\"";
+	txt += url;
+	txt +=  "\"class=\"img-fluid\" alt =\"";
+	txt += alt;
+	txt += "\"><p>";
+	txt += info;
+	txt+="</p></div>";
+}
+
+document.getElementById('addImg').innerHTML = txt;
+
+
+
